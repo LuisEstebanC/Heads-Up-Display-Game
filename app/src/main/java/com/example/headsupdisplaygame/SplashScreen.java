7c7ad42cb7b789d9a422879084627a024fb715eb
LuisEstebanC <ing.29capellan@gmailcom>
2020-02-24 18:02:06 -0400

    Final SplashScreen.

--- app/src/main/java/com/example/headsupdisplaygame/SplashScreen.java
+++ app/src/main/java/com/example/headsupdisplaygame/SplashScreen.java
@@ -2,16 +2,29 @@ package com.example.headsupdisplaygame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
+    //tiempo dce duracion del splash
+    private final int DURACION_SPLASH = 2000; // 2 segundos
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(this,MainActivity.class ));
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+
+                startActivity(new Intent(SplashScreen.this,MainActivity.class ));
+                finish();
+
+            };
+        }, DURACION_SPLASH);
+
+
+
     }
 }
